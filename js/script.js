c04388a6bc23cfe0425514c2f8ddc6cc7b1458dd
Aina2bean <77223940+Aina2bean@users.noreply.github.com>
2025-12-16 23:58:08 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var map = L.map('map').setView([35.68111, 139.76667], 18);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const route = [
+  [35.681236, 139.767125], // 東京駅
+  [35.67016, 139.76359]
+];
+
+L.polyline(route).addTo(map);
